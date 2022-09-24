8631e439650f6822f28971c611002ca92775f64e
Hadeer-reda-sakr <60269940+Hadeer-reda-sakr@users.noreply.github.com>
2022-09-24 19:58:45 +0200

    Add files via upload

---
+++ book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookstore.Models
+{
+    public class book
+    {
+        public string title { get; set; }
+
+        public int price { get; set; }
+
+        public string author { get; set; }
+
+    }
+
+
+}
+
---
+++ bookController.cs
@@ -0,0 +1,25 @@
+﻿using bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookstore.Controllers
+{
+    public class bookController : Controller
+    {
+      public IActionResult index()
+        {
+            List<book> books = new List<book>
+            {
+                new book(){title ="Asp.net " , price = 50, author = "mohamed"} ,
+                new book (){title = "c#" , price = 880,author="ahmed"},
+                new book(){title ="sQl " , price = 900, author = "ali"} ,
+
+            };
+            return View(books);
+        } 
+        
+    }
+}
